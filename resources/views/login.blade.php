c5b12547dc8bd6f2b40cec83ca96e1a604477e18
munawir4 <munawir4@gmail.com>
2021-10-01 11:27:38 +0530

    new

---
+++ resources/views/login.blade.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body align="center" cellpadding="20">
+    <div class="bg pd">
+        <table align="center" cellpadding="10">
+            <h1>login Here</h1>
+            <img src="pes1.jpg" alt="">
+
+            <form action="" method="post">
+                <tr>
+                    <td>
+                        <label for="">Enter Name</label>
+                    </td>
+                    <td>
+                        <input type="text" name="" id="" required></td>
+                </tr>
+
+
+
+                <tr>
+                    <td>
+                        <label for="">Date of Birth</label>
+                    </td>
+
+                    <td>
+                        <input type="date" name="" id=""></td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="">Age Range</label>
+                    </td>
+                    <td>
+                        <select name="" id="">
+                        <option value=""></option>
+                        <option value="">>12</option>
+                        <option value="">12-18</option>
+                        <option value="">18-25</option>
+                    </select>
+
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="">Gender</label>
+                    </td>
+                    <td>
+                        <input type="radio" name="gender" id="">Male
+                        <input type="radio" name="gender" id="">Female
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <label for="">Email Adress</label>
+
+                    </td>
+                    <td>
+                        <textarea name="" id="" cols="20" rows="3"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button>Create Account</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button> <a href="http://127.0.0.1:8000/pes">Back</a></button>
+
+                    </td>
+                </tr>
+
+
+
+
+
+        </table>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/pes.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+</head>
+
+<body align="center" cellpadding="200">
+    <div class="bg img">
+        <form action="" method="get"></form>
+        <h1>PES EFOOTBALL</h1>
+        <img src="pes.jpg" alt="">
+        <br>
+        <br>
+        <button class="color"><a href="http://127.0.0.1:8000/login"> LOGIN HERE</a></button>
+        </form>
+
+
+    </div>
+</body>
+
+</html>
