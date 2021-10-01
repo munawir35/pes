a455118eb38af7048059b3bf4ad650c1593d3596
munawir4 <munawir4@gmail.com>
2021-10-01 12:03:01 +0530

    new

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,36 +10,35 @@
 </head>
 
 <body align="center" cellpadding="20">
-    <div class="bg pd">
-        <table align="center" cellpadding="10">
-            <h1>login Here</h1>
-            <img src="pes1.jpg" alt="">
+    <table align="center" cellpadding="10">
+        <h1>login Here</h1>
+        <img src="pes1.jpg" alt="">
 
-            <form action="" method="post">
-                <tr>
-                    <td>
-                        <label for="">Enter Name</label>
-                    </td>
-                    <td>
-                        <input type="text" name="" id="" required></td>
-                </tr>
-
+        <form action="" method="post">
+            <tr>
+                <td>
+                    <label for="">Enter Name</label>
+                </td>
+                <td>
+                    <input type="text" name="" id="" required></td>
+            </tr>
 
 
-                <tr>
-                    <td>
-                        <label for="">Date of Birth</label>
-                    </td>
 
-                    <td>
-                        <input type="date" name="" id=""></td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="">Age Range</label>
-                    </td>
-                    <td>
-                        <select name="" id="">
+            <tr>
+                <td>
+                    <label for="">Date of Birth</label>
+                </td>
+
+                <td>
+                    <input type="date" name="" id=""></td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Age Range</label>
+                </td>
+                <td>
+                    <select name="" id="">
                         <option value=""></option>
                         <option value="">>12</option>
                         <option value="">12-18</option>
@@ -47,45 +46,45 @@
                     </select>
 
 
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="">Gender</label>
-                    </td>
-                    <td>
-                        <input type="radio" name="gender" id="">Male
-                        <input type="radio" name="gender" id="">Female
-                    </td>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Gender</label>
+                </td>
+                <td>
+                    <input type="radio" name="gender" id="">Male
+                    <input type="radio" name="gender" id="">Female
+                </td>
 
-                </tr>
-                <tr>
-                    <td>
-                        <label for="">Email Adress</label>
+            </tr>
+            <tr>
+                <td>
+                    <label for="">Email Adress</label>
 
-                    </td>
-                    <td>
-                        <textarea name="" id="" cols="20" rows="3"></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <button>Create Account</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <button> <a href="http://127.0.0.1:8000/pes">Back</a></button>
+                </td>
+                <td>
+                    <textarea name="" id="" cols="20" rows="3"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button>Create Account</button>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button> <a href="http://127.0.0.1:8000/pes">Back</a></button>
 
-                    </td>
-                </tr>
-
-
+                </td>
+            </tr>
 
 
 
-        </table>
-        </form>
+
+
+    </table>
+    </form>
     </div>
 </body>
 
